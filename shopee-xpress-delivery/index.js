4958787bf135025c6718cb6cd06b2e52affa1a8a
Erik <erikhau3010@gmail.com>
2022-04-19 10:01:24 +0700

    refactor: algorithm

--- shopee-xpress-delivery/index.js
+++ shopee-xpress-delivery/index.js
@@ -12,8 +12,6 @@ const solution = (array) => {
   ];
 
   const destination = { y: y - 1, x: x - 1 };
-  const queue = [{ y: 0, x: 0 }];
-  const stack = [];
   const visited = Array.from({ length: board.length }, (e) =>
     Array.from({ length: board[0].length }, (e) => false)
   );
@@ -29,88 +27,115 @@ const solution = (array) => {
     )
   );
 
+  // TODO: Using BFS algorithm
+  const validRoutes = ([dirY, dirX]) =>
+    dirY >= 0 && dirX >= 0 && dirY < board.length && dirX < board[0].length;
+  const queue = [{ y: 0, x: 0 }];
+  // const stack = [];
   const getAvailableRoute = ({ y, x }) => {
-    const validRoutes = ([dirY, dirX]) =>
-      dirY >= 0 && dirX >= 0 && dirY < board.length && dirX < board[0].length;
-
     return directions
       .map(([dirY, dirX]) => [y + dirY, x + dirX])
       .filter(validRoutes);
   };
 
-  while (queue.length) {
-    const { y: curY, x: curX } = queue.shift();
-    visited[curY][curX] = true;
+  // while (queue.length) {
+  //   const { y: curY, x: curX } = queue.shift();
+  //   visited[curY][curX] = true;
 
-    if (curY === destination.y && curX === destination.x) break;
+  //   if (curY === destination.y && curX === destination.x) break;
 
-    const routes = getAvailableRoute({ y: curY, x: curX });
-    for (const [routeY, routeX] of routes) {
-      if (visited[routeY][routeX]) continue;
+  //   const routes = getAvailableRoute({ y: curY, x: curX });
+  //   for (const [routeY, routeX] of routes) {
+  //     if (visited[routeY][routeX]) continue;
 
-      stack.push({
-        from: [curY, curX],
-        value: [routeY, routeX],
-        isTeleported: false,
-      });
-      queue.push({ y: routeY, x: routeX });
-      visited[routeY][routeX] = true;
+  //     stack.push({
+  //       from: [curY, curX],
+  //       value: [routeY, routeX],
+  //       isTeleported: false,
+  //     });
+  //     queue.push({ y: routeY, x: routeX });
+  //     visited[routeY][routeX] = true;
 
-      if (!board[curY][curX]) continue;
+  //     if (!board[curY][curX]) continue;
 
-      const blackHole = blackHoles[board[curY][curX]];
-      for (const { y: blackHoleY, x: blackHoleX } of blackHole) {
-        if (visited[blackHoleY][blackHoleX]) continue;
+  //     const blackHole = blackHoles[board[curY][curX]];
+  //     for (const { y: blackHoleY, x: blackHoleX } of blackHole) {
+  //       if (visited[blackHoleY][blackHoleX]) continue;
 
-        stack.push({
-          from: [curY, curX],
-          value: [blackHoleY, blackHoleX],
-          isTeleported: true,
-        });
-        queue.push({ y: blackHoleY, x: blackHoleX });
-        visited[blackHoleY][blackHoleX] = true;
+  //       stack.push({
+  //         from: [curY, curX],
+  //         value: [blackHoleY, blackHoleX],
+  //         isTeleported: true,
+  //       });
+  //       queue.push({ y: blackHoleY, x: blackHoleX });
+  //       visited[blackHoleY][blackHoleX] = true;
+  //     }
+  //   }
+  // }
+
+  // const buildPath = () => {
+  //   const path = [];
+
+  //   let from = [destination.y, destination.x];
+  //   while (from) {
+  //     const parent = stack.find(
+  //       ({ from: _, value }) => value.toString() === from.toString()
+  //     );
+
+  //     parent &&
+  //       path.push({ value: parent.value, isTeleported: parent.isTeleported });
+  //     from = parent?.from ?? null;
+  //   }
+
+  //   return path;
+  // };
+
+  // return buildPath().reduce((total, item) => {
+  //   if (!item.isTeleported) total++;
+  //   return total;
+  // }, 0);
+
+  const floodFill = () => {
+    const stepCount = Array.from({ length: board.length }, (e) =>
+      Array.from({ length: board[0].length }, (e) => 0)
+    );
+    visited[0][0] = true;
+
+    while (true) {
+      const { y: curY, x: curX } = queue.shift();
+
+      if (curY === destination.y && curX === destination.x) break;
+
+      for (const [dirY, dirX] of directions) {
+        const newPos = { y: curY + dirY, x: curX + dirX };
+
+        if (!validRoutes([newPos.y, newPos.x]) || visited[newPos.y][newPos.x])
+          continue;
+
+        queue.push({ y: newPos.y, x: newPos.x });
+        stepCount[newPos.y][newPos.x] = stepCount[curY][curX] + 1;
+        visited[newPos.y][newPos.x] = true;
+
+        if (!board[curY][curX]) continue;
+
+        for (const blackHole of blackHoles[board[curY][curX]]) {
+          if (
+            !validRoutes([blackHole.y, blackHole.x]) ||
+            visited[blackHole.y][blackHole.x]
+          )
+            continue;
+
+          queue.push({ y: blackHole.y, x: blackHole.x });
+          stepCount[blackHole.y][blackHole.x] = stepCount[curY][curX];
+          visited[blackHole.y][blackHole.x] = true;
+        }
       }
     }
-  }
-
-  const buildPath = () => {
-    const path = [];
-
-    let from = [destination.y, destination.x];
-    while (from) {
-      const parent = stack.find(
-        ({ from: _, value }) => value.toString() === from.toString()
-      );
-
-      parent &&
-        path.push({ value: parent.value, isTeleported: parent.isTeleported });
-      from = parent?.from ?? null;
-    }
-
-    return path;
+    return stepCount[destination.y][destination.x];
   };
-
-  return buildPath().reduce((total, item) => {
-    if (!item.isTeleported) total++;
-    return total;
-  }, 0);
+  return floodFill();
 
   // TODO: Using flood fill algorithm
-  // const blackHoles = {};
-  // board.forEach((row, yIndex) =>
-  //   row.forEach(
-  //     (item, xIndex) =>
-  //       item !== 0 &&
-  //       (blackHoles[item] = [
-  //         ...(blackHoles[item] ?? []),
-  //         { y: yIndex, x: xIndex },
-  //       ])
-  //   )
-  // );
-
-  // const visited = Array.from({ length: board.length }, (e) =>
-  //   Array.from({ length: board[0].length }, (e) => false)
-  // );
   // const stepCount = Array.from({ length: board.length }, (e) =>
   //   Array.from({ length: board[0].length }, (e) => 0)
   // );
@@ -120,14 +145,8 @@ const solution = (array) => {
   // while (true) {
   //   const [curY, curX] = nextSteps.shift();
 
-  //   if (curY === y - 1 && curX === x - 1) break;
+  //   if (curY === destination.y && curX === destination.x) break;
 
-  //   const directions = [
-  //     [0, 1],
-  //     [0, -1],
-  //     [1, 0],
-  //     [-1, 0],
-  //   ];
   //   for (const [dirY, dirX] of directions) {
   //     const newPos = { y: curY + dirY, x: curX + dirX };
 
@@ -141,30 +160,30 @@ const solution = (array) => {
   //       continue;
 
   //     nextSteps.push([newPos.y, newPos.x]);
-  //     stepCount[newPos.y][newPos.x] = scurYt[curXepCount[curY][curX] + 1;
+  //     stepCount[newPos.y][newPos.x] = stepCount[curY][curX] + 1;
   //     visited[newPos.y][newPos.x] = true;
 
-  //     if (board[curY][curX] !== 0) {
-  //       for (const blackHole of blackHoles[board[curY][curX]]) {
-  //         if (
-  //           blackHole.y < 0 ||
-  //           blackHole.x < 0 ||
-  //           blackHole.y >= y ||
-  //           blackHole.x >= x ||
-  //           visited[blackHole.y][blackHole.x]
-  //         )
-  //           continue;
+  //     if (!board[curY][curX]) continue;
 
-  //         nextSteps.push([blackHole.y, blackHole.x]);
-  //         stepCount[blackHole.y][blackHole.x] = stepCount[curY][curX];
-  //         visited[blackHole.y][blackHole.x] = true;
-  //       }
+  //     for (const blackHole of blackHoles[board[curY][curX]]) {
+  //       if (
+  //         blackHole.y < 0 ||
+  //         blackHole.x < 0 ||
+  //         blackHole.y >= y ||
+  //         blackHole.x >= x ||
+  //         visited[blackHole.y][blackHole.x]
+  //       )
+  //         continue;
+
+  //       nextSteps.push([blackHole.y, blackHole.x]);
+  //       stepCount[blackHole.y][blackHole.x] = stepCount[curY][curX];
+  //       visited[blackHole.y][blackHole.x] = true;
   //     }
   //   }
   // }
 
-  // return stepCount;
-  // return stepCount[y - 1][x - 1];
+  // // return stepCount;
+  // return stepCount[destination.y][destination.x];
 };
 
 console.log(
